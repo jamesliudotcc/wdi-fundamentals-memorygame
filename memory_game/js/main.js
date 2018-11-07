89983a2a60774786d24790b7fa8715cdc53ad410
James Liu <james@jamesliu.cc>
2018-11-07 10:53:55 -0800

    Changed cards to objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-console */
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  { rank: 'queen', suit: 'hearts', cardImage: 'images/queen-of-hearts.png' },
+  {
+    rank: 'queen',
+    suit: 'diamonds',
+    cardImage: 'images/queen-of-diamonds.png',
+  },
+  { rank: 'king', suit: 'hearts', cardImage: 'images/king-of-hearts.png' },
+  { rank: 'king', suit: 'diamonds', cardImage: 'images/king-of-diamonds.png' },
+];
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -11,8 +20,10 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-  console.log('User flipped ' + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log('User flipped ' + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId]).rank;
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
   checkForMatch();
 }
 flipCard(0);
